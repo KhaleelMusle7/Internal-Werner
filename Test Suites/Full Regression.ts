7e567578a908de28b124a21fcbf880c252fa7aba
Xng-Anas <anas.salahat@xngage.com>
2020-11-18 19:31:31 +0200

    Structural Enhancements

--- Test Suites/Full Regression.ts
+++ Test Suites/Full Regression.ts
@@ -15,36 +15,36 @@
       <guid>27ba92ba-f343-429b-b5e5-9d1681682bbf</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Checkout/Ready Test Cases/CheckoutFromList-OneTimeShippingWithSavedCard</testCaseId>
+      <testCaseId>Test Cases/Checkout Scenarios/CheckoutFromList-OneTimeShippingWithSavedCard</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>223c8598-4a7a-4f94-8c82-9ca2a4dee781</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Checkout/Ready Test Cases/CheckoutFromPDP - ShippingAddressWithNewCC</testCaseId>
+      <testCaseId>Test Cases/Checkout Scenarios/CheckoutFromPDP - ShippingAddressWithNewCC</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>b6a5355c-7642-41de-876c-f71e35ab4e0f</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Checkout/Ready Test Cases/CheckoutFromPLP - OneTimeShippingAddressWithPO</testCaseId>
+      <testCaseId>Test Cases/Checkout Scenarios/CheckoutFromPLP - OneTimeShippingAddressWithPO</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>a26fb5b3-2e40-41e4-af4f-b6103ea570c6</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Checkout/Ready Test Cases/CheckoutFromQuickOrder-ShippingAddressWithPO</testCaseId>
+      <testCaseId>Test Cases/Checkout Scenarios/CheckoutFromQuickOrder-ShippingAddressWithPO</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>05087d6a-0317-412e-a43e-c28075b2c004</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Checkout/Ready Test Cases/CheckoutFromQuickOrderModal-ShippingAddressWithSaveNewCard</testCaseId>
+      <testCaseId>Test Cases/Checkout Scenarios/CheckoutFromQuickOrderModal-ShippingAddressWithSaveNewCard</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>008353c7-82a4-4a73-a435-e8cdbc4c92e1</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Checkout/Ready Test Cases/CheckoutFromSavedOrder-ShippingAddressWithNewCC</testCaseId>
+      <testCaseId>Test Cases/Checkout Scenarios/CheckoutFromSavedOrder-ShippingAddressWithNewCC</testCaseId>
    </testCaseLink>
 </TestSuiteEntity>
